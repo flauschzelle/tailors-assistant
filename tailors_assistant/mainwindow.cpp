a547d9345cbc38ccfde478e639e6f9260061f6ff
Kirstin Rohwer <k.rohwer@tu-bs.de>
2019-06-16 01:21:15 +0200

    defined two input modes and changing some ui properties according to input mode

--- tailors_assistant/mainwindow.cpp
+++ tailors_assistant/mainwindow.cpp
@@ -1,14 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+// constructor
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setInputMode(record); //default value for input mode
 }
 
+// destructor
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// sets the mode for record input or offer calculation
+void MainWindow::setInputMode(InputMode mode)
+{
+    this->mode = mode;
+
+    if (mode == record)
+    {
+        ui->pieceDataBox->setTitle("Werkstück-Stammdaten");
+        ui->filterWidget->setVisible(false);
+        ui->pictureWidget->setVisible(true);
+        ui->deletePiecePushButton->setText("Werkstück löschen");
+    }
+
+    if (mode == offer)
+    {
+        ui->pieceDataBox->setTitle("Angebots-Stammdaten");
+        ui->filterWidget->setVisible(true);
+        ui->pictureWidget->setVisible(false);
+        ui->deletePiecePushButton->setText("Angebot löschen");
+    }
 }
--- tailors_assistant/mainwindow.h
+++ tailors_assistant/mainwindow.h
@@ -7,6 +7,8 @@ namespace Ui {
 class MainWindow;
 }
 
+enum InputMode { record, offer };
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -15,8 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void setInputMode(InputMode mode);
+
 private:
     Ui::MainWindow *ui;
+
+    InputMode mode;
 };
 
 #endif // MAINWINDOW_H
